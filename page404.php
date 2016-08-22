e56b399711d471f3c4f8e399e514f7e9a3a6d55c
findomba <lucyindomba@gmail.com>
2016-08-22 15:59:17 -0400

    Gestion page 404

---
+++ page404.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Agence DIGIKAN |page erreur</title>
+    <!--<link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">-->
+    <link href="css/bootstrap.css" rel="stylesheet">
+    <link href="css/bootstrap-responsive.css" rel="stylesheet">
+
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,600 Merriweather|Pacifico' rel='stylesheet' type='text/css'>
+    <link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css">
+
+    <!-- PUT THE STYLESHEET LINK HERE -->
+    <link rel="stylesheet" href="css/final.css">
+</head>
+<body>
+
+
+<h1> OUPS CETTE PAGE N'EXISTE PLUS </h1>
+<p>REVIENS VERS NOUS <a href="accueil.php">SUR NOTRE SITE</a></p>
+
+
+</body>
+</html>
