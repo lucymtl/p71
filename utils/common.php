cf8e1eecc7ccbdb6b05275d377e5fef27e783a2e
findomba <lucyindomba@gmail.com>
2016-08-24 12:49:59 -0400

    Ajout idem

---
+++ utils/common.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: findomba
+ * Date: 2016-08-24
+ * Time: 09:38
+ */
+/* Savoir si une session est démarrée */
+function is_session_started()
+{
+//  echo phpversion();
+    if ( php_sapi_name() !== 'cli' ) {
+        if ( version_compare(phpversion(), '5.4.0', '>=') ) {
+            return session_status() === PHP_SESSION_ACTIVE ? TRUE : FALSE;
+        } else {
+            return session_id() === '' ? FALSE : TRUE;
+        }
+    }
+    return FALSE;
+}
+/**
+ * Traiter différemment l'encodage des chaines suivant la version de PHP
+ * Workaround pb encodage sur Sentora vs local
+ */
+function get_right_encoding($str)
+{
+    return version_compare(phpversion(), '5.4.0', '>=') ? $str : utf8_encode($str);
+}
---
+++ utils/login_out.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: findomba
+ * Date: 2016-08-24
+ * Time: 09:38
+ */
