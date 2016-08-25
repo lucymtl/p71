25539b21890b6221698c255e029bce6fbc0442ff
findomba <lucyindomba@gmail.com>
2016-08-25 12:16:10 -0400

    Page accueil positionement final+ responsive ok

--- accueil.php
+++ accueil.php
@@ -14,45 +14,16 @@
 
   <!-- PUT THE STYLESHEET LINK HERE -->
   <link rel="stylesheet" href="css/final.css">
+<!--  <link rel="stylesheet" href="css/test.css">-->
+  <!--[if lt IE 9]>
+  <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+  <![endif]-->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+
 </head>
 <body>
 
-<!-- HEADER -->
-<!--<section>-->
-<!--  <div class="col-lg-4 col-md-2 col-sm-2 col-xs-2 "><a href="accueil.php"><img src="images/logo_bfw.png" alt="logo digikan" /></a></div>-->
-<!--  <div class=" col-lg-8 col-md-10 col-sm-10 navbar">-->
-<!--    <div class="navbar-inner">-->
-<!---->
-<!--      <!-- .btn-navbar is used as the toggle for collapsed navbar content -->
-<!--      <a class="col-md-10  col-sm-10 col-sm-offset-2 col-xs-12 btn navbar btn-navbar collapse" data-toggle="collapse" data-target=".nav-collapse">-->
-<!---->
-<!--        <span class="icon-bar">MENU</span>-->
-<!--        <span class="icon-bar"></span>-->
-<!--        <span class="icon-bar"></span>-->
-<!---->
-<!--      </a>-->
-<!---->
-<!--      <!-- Be sure to leave the brand out there if you want it shown -->
-<!--      <!--<a class="brand" href="#">DIGIKAN</a>-->
-<!---->
-<!--      <!-- Everything you want hidden at 940px or less, place within here -->
-<!--      <div class=" col-lg-offset-3 nav-collapse">-->
-<!--        <nav >-->
-<!--          <ul class=" nav navbar-nav">-->
-<!--            <li class=""><a href="accueil.php">ACCUEIL</a></li>-->
-<!--            <li class=""><a href="apropos.php">A PROPOS</a></li>-->
-<!--            <li><a href="service.php">NOS SERVICES</a></li>-->
-<!--            <li><a href="realisations.php">REALISATIONS</a></li>-->
-<!--            <li><a href="boutique.php">BOUTIQUE</a></li>-->
-<!--            <li><a href="nouscontacter.php">CONTACT</a></li>-->
-<!--          </ul>-->
-<!--        </nav>-->
-<!--      </div>-->
-<!---->
-<!--    </div>-->
-<!--  </div>-->
-<!---->
-<!--</section>-->
+
 <?php
 include ('includes/header.php'); ?>
 
@@ -61,14 +32,21 @@ include ('includes/header.php'); ?>
 
 
 <section  id="mainindex" class="row">
-  <div class="index col-lg-12">
+  <div class="index ">
     <div class=" expertise col-lg-4">
       <div class="detail">
         <img src="images/logo_bfw.png" alt="logo client">
         <h2> Campagnes de publicités</h2>
         <p>Rédaction de contenu pour l’image de marque
           Réalisation de capsules vidéos
-          Affichage publicitaire BLAAAAAAAAAAAAAAAAAAAAAAAAAAA
+          Affichage publicitaire.
+          Affichage publicitaire.
+          Affichage publicitaire.
+          Affichage publicitaire.
+          Affichage publicitaire.
+          Affichage publicitaire.
+          Affichage publicitaire.
+
         </p>
         <a class="button" href="realisations.php">DECOUVRIR</a>
       </div>
@@ -105,6 +83,8 @@ include ('includes/header.php'); ?>
           Analyse, évaluation, conseils et recommandations,
           Conception d’une stratégie web 2.0 en fonction de votre secteur et de votre positionnement,
           Mise en place et intégration.
+          Mise en place et intégration.
+          Mise en place et intégration.
         </p>
         <a class="button" href="realisations.php">DECOUVRIR</a>
       </div>
@@ -118,42 +98,24 @@ include ('includes/header.php'); ?>
 <section></section>
 <div class="row">
   <section  id="apropos1" class="clientslogo col-lg-12">
-    <h2>ILS NOUS ONT FAIT CONFIANCE</h2>
+    <h3>ILS NOUS ONT FAIT CONFIANCE</h3>
     <div class="row">
-      <div class="col-lg-3 col-xs-6"><img src="images/logo_bfw.png" alt="logo client"></div>
-      <div class="col-lg-3 col-xs-6"><img src="images/logo_bfw.png" alt="logo client"></div>
-      <div class="col-lg-3 col-xs-6"><img src="images/logo_bfw.png" alt="logo client"></div>
-      <div class="col-lg-3 col-xs-6"><img src="images/logo_bfw.png" alt="logo client"></div>
+      <div class="clients col-lg-4"><img src="images/logo_bfw.png" alt="logo client"></div>
+      <div class="clients col-lg-4"><img src="images/logo_bfw.png" alt="logo client"></div>
+      <div class="clients col-lg-4"><img src="images/logo_bfw.png" alt="logo client"></div>
+      <div class="clients col-lg-4"><img src="images/logo_bfw.png" alt="logo client"></div>
+      <div class="clients col-lg-4"><img src="images/logo_bfw.png" alt="logo client"></div>
+      <div class="clients col-lg-4"><img src="images/logo_bfw.png" alt="logo client"></div>
 
     </div>
   </section>
 </div>
-<section></section>
+<section class="space"></section>
 
-<!--<section class="contact">-->
-<!--  <div class="content-wrap">-->
-<!--    <div class="social">-->
-<!--      <a href="#" title="Twitter"><i class="fa fa-twitter"></i></a>-->
-<!--      <a href="#" title="LinkedIn"><i class="fa fa-linkedin"></i></a>-->
-<!--      <a href="#" title="Instagram"><i class="fa fa-instagram"></i></a>-->
-<!--      <a href="#" title="Facebook"><i class="fa fa-facebook"></i></a>-->
-<!--    </div>-->
-<!---->
-<!--    <div><img src="images/logo_bfw.png" alt="logo client"></div>-->
-<!---->
-<!--    <p><a href="mailto:hello@your-email.com">hello@digikan.com</a></p>-->
-<!---->
-<!---->
-<!--    <footer>-->
-<!--      <p class="copyright">Copyright &#169; 2016 by Lucy Ind</p>-->
-<!--    </footer>-->
-<!--  </div>-->
-<!--</section>-->
 
 
 <?php
 include ('includes/footer.php'); ?>
-
 
 
 
