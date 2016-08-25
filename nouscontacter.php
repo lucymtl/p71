878f7477f68d4d0191eef63eced853bd8c68f776
findomba <lucyindomba@gmail.com>
2016-08-25 14:39:03 -0400

    responsive ok +menu

--- nouscontacter.php
+++ nouscontacter.php
@@ -93,19 +93,7 @@ if ($receiving) {
 //    }
 //}
 
-//// Le champ sexe
-//$sexe = ""; // Contenu du champ sexe (sexe_h ou sexe_f)
-//$sexe_valide = true; // Le champ est valide par défaut
-//$sexe_msg_validation = ''; // Le message à renvoyer à l'utilisateur si le sexe n'est pas coché
-//if ($receiving) {
-//    $sexe_valide = array_key_exists('sexe', $_POST)
-//        && in_array($_POST['sexe'], array('sexe_h', 'sexe_f')); // Le champ est valide par défaut
-//    if ($sexe_valide) {
-//        $sexe = $_POST['sexe'];
-//    } else {
-//        $sexe_msg_validation = "Le sexe n'est pas indiqué";
-//    }
-//}
+
 
 /************Pour le renvoi vers la boite gmail************************************/
 ////form validation vars
@@ -226,6 +214,11 @@ if ($receiving) {
 
     <!-- PUT THE STYLESHEET LINK HERE -->
     <link rel="stylesheet" href="css/final.css">
+    <!--  <link rel="stylesheet" href="css/test.css">-->
+    <!--[if lt IE 9]>
+    <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
 </head>
 <body>
 
@@ -233,43 +226,8 @@ if ($receiving) {
 
 <?php
 include ('includes/header.php'); ?>
-<!--<section>-->
-<!--    <div class="col-lg-4 col-md-2 col-sm-2 col-xs-2 "><a href="accueil.php"><img src="images/logo_bfw.png" alt="logo digikan" /></a></div>-->
-<!--    <div class=" col-lg-8 col-md-10 col-sm-10 navbar">-->
-<!--        <div class="navbar-inner">-->
-<!---->
-<!--            <!-- .btn-navbar is used as the toggle for collapsed navbar content -->-->
-<!--            <a class="col-md-10  col-sm-10 col-sm-offset-2 col-xs-12 btn navbar btn-navbar collapse" data-toggle="collapse" data-target=".nav-collapse">-->
-<!---->
-<!--                <span class="icon-bar">MENU</span>-->
-<!--                <span class="icon-bar"></span>-->
-<!--                <span class="icon-bar"></span>-->
-<!---->
-<!--            </a>-->
-<!---->
-<!--            <!-- Be sure to leave the brand out there if you want it shown -->-->
-<!--            <!--<a class="brand" href="#">DIGIKAN</a>-->-->
-<!---->
-<!--            <!-- Everything you want hidden at 940px or less, place within here -->-->
-<!--            <div class=" col-lg-offset-3 nav-collapse">-->
-<!--                <nav >-->
-<!--                    <ul class=" nav navbar-nav">-->
-<!--                        <li class=""><a href="accueil.php">ACCUEIL</a></li>-->
-<!--                        <li><a href="apropos.php">A PROPOS</a></li>-->
-<!--                        <li><a href="service.php">NOS SERVICES</a></li>-->
-<!--                        <li><a href="realisations.php">REALISATIONS</a></li>-->
-<!--                        <li><a href="nouscontacter.php">CONTACT</a></li>-->
-<!--                        <li><a href="Ecommerce/boutique.php">BOUTIQUE</a></li>-->
-<!--                    </ul>-->
-<!--                </nav>-->
-<!--            </div>-->
-<!---->
-<!--        </div>-->
-<!--    </div>-->
-<!---->
-<!--</section>-->
 
-<section></section>
+
 
 <section class="row"></section>
 
